245484cd57f8011ebf97dd6efb9eea555c46a5e8
Tim Slippy, voxelv <slippytim@gmail.com>
2018-06-28 00:18:22 -0700

    added recipes

--- src/main/java/com/voxelv/glowstone_torch/init/BlockInit.java
+++ src/main/java/com/voxelv/glowstone_torch/init/BlockInit.java
@@ -3,6 +3,7 @@ package com.voxelv.glowstone_torch.init;
 import com.voxelv.glowstone_torch.objects.blocks.BlockBase;
 import com.voxelv.glowstone_torch.objects.blocks.BlockGlowstoneTorch;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 import java.util.ArrayList;
@@ -11,6 +12,6 @@ import java.util.List;
 public class BlockInit {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-    public static final Block BLOCK_GLOWSTONE_SOLIDIFIED = new BlockBase("block_glowstone_solidified", Material.ROCK).setLightLevel(1.0f);
-    public static final Block BLOCK_GLOWSTONE_TORCH = new BlockGlowstoneTorch("block_glowstone_torch").setLightLevel(1.0f);
+    public static final Block BLOCK_GLOWSTONE_SOLIDIFIED = new BlockBase("block_glowstone_solidified", Material.ROCK).setLightLevel(1.0F).setHardness(2.0F).setResistance(10.0F);
+    public static final Block BLOCK_GLOWSTONE_TORCH = new BlockGlowstoneTorch("block_glowstone_torch").setLightLevel(1.0F).setHardness(0.4F);
 }
